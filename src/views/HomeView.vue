dec666472bc3de86b0791c7e899037556c7efa86
William Maphanga <williesdk@gmail.com>
2022-07-30 17:02:57 +0200

    added filter input for pokemons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,19 @@
 <script setup>
 import PokemonCard from "../components/PokemonCard.vue";
-import { onMounted } from "@vue/runtime-core";
+import { onMounted, computed } from "@vue/runtime-core";
 import { storeToRefs } from "pinia";
 import { usePokemonStore } from "../stores/pokemon";
 import SpinnerLoader from "../components/SpinnerLoader.vue";
+import { ref } from "vue";
 
 const pokemonStore = usePokemonStore();
 const { errorMessage, loading, pokemons } = storeToRefs(pokemonStore);
+const searchText = ref("");
+const filteredPokemons = computed(() =>
+  pokemons.value.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(searchText.value);
+  })
+);
 
 onMounted(() => {
   pokemonStore.getPokemons();
@@ -19,8 +26,17 @@ onMounted(() => {
   </div>
   <h1 v-if="errorMessage != null">{{ errorMessage }}</h1>
 
+  <div v-if="!loading" class="form-group" style="padding-bottom: 1rem">
+    <input
+      class="form-control"
+      type="text"
+      placeholder="Search…"
+      v-model="searchText"
+    />
+  </div>
+
   <PokemonCard
-    v-for="pokemon in pokemons"
+    v-for="pokemon in filteredPokemons"
     :pokemon="pokemon"
     :key="pokemon"
   ></PokemonCard>
